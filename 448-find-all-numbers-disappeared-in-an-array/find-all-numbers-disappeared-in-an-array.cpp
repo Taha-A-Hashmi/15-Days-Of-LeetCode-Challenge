81d2f44c76dc354ea5b766b905d3536d5214fbb9
Taha-A-Hashmi <taha.a.hashmi@gmail.com>
2024-11-27 22:25:31 +0500

    Time: 74 ms (11.01%) | Memory: 78.1 MB (5.00%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -4,24 +4,26 @@ public:
         unordered_set<int> ans;
         int n = nums.size();
 
-        //Populating the set with all the values from 1 to n
+        //Populating the set with all the values from 1 to n O(n)
         for (int i = 1; i <= nums.size(); i++) {
             ans.insert(i);
         }
 
-        // Removing the values that exist in the array from the set
+        // Removing the values that exist in the array from the set O(n)
         for (int i = 0; i < nums.size(); i++) {
-            if (ans.find(nums[i]) != ans.end()) {
+            if (ans.find(nums[i]) != ans.end()) { // These lookups are all 0(1) because the use of hashtable
                 ans.erase(nums[i]);
             }
         }
 
-        // Populating and returning an array with the remaining set values
+        // Populating and returning an array with the remaining set values O(n)
         vector<int> ans2;
         for (int x : ans) {
             ans2.push_back(x);
         }
 
         return ans2;
+
+        //Overall time complexity: O(n)
     }
 };
